3b2a4cb43c20c238436c640c6962e1b2ad19679d
Dmitriy Kanaev <kanaev.dm@mail.ru>
2020-04-09 15:06:47 +0300

    add failed Square test

---
+++ src/test/java/ru/stqa/pft/addressbook/sandboxtests/SquareTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.sandboxtests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.sandbox.Square;
+
+import static org.testng.Assert.assertEquals;
+
+public class SquareTests {
+
+    @Test
+    public void testArea() {
+        Square square = new Square(5.0);
+        assertEquals(square.area(), 20.0);
+    }
+}
